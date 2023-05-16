dcc0c51be2de7e2783ddf5c2e0e2263c7f8c088d
Joshua Frazer <77747282+BlackOutDevelops@users.noreply.github.com>
2023-05-16 18:14:27 -0400

    Switch email provided since I shutdown namecheap server

--- Portfolio/Controllers/EmailController.cs
+++ Portfolio/Controllers/EmailController.cs
@@ -9,28 +9,22 @@ namespace Portfolio.Controllers
     [ApiController]
     [Route("[controller]")]
     public class EmailController : ControllerBase
-    {
-        private readonly IConfiguration _config;
-
-        public EmailController(IConfiguration config)
-        {
-            _config = config;
-        }
-    
-            [HttpPost]
+    {   
+        [HttpPost]
         public void Post(Email email)
         {
             string to = "joshiefrazie@hotmail.com";
-            string from = "joshiefrazie@joshua-frazer.com";
+            string from = "joshiefrazie@hotmail.com";
             string subject = "Email From Portfolio Website";
             string body = "Contact's Email Address: " + email.EmailAddress + 
                           "\nContact's Phone Number: " + email.PhoneNumber + "\n---------------------------------------------------------------------------------------\n" +
                           email.Message;
             MailMessage message = new MailMessage(new MailAddress(from, "Portfolio Message").ToString(), to, subject, body);
 
-            SmtpClient smtpClient = new SmtpClient("joshua-frazer.com");
+            SmtpClient smtpClient = new SmtpClient("smtp.office365.com");
             smtpClient.UseDefaultCredentials = false;
-            smtpClient.Credentials = new NetworkCredential(from, _config["EmailPassword"]);
+            smtpClient.Credentials = new NetworkCredential(from, Environment.GetEnvironmentVariable("EmailPassword"));
+            smtpClient.TargetName = "STARTTLS/smtp.office365.com";
             smtpClient.Port = 587;
             smtpClient.EnableSsl = true;
 
