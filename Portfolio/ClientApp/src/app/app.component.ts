c36f70c47f5a20d7070e86a1e7e64d76d7e4b0be
Joshua Frazer <joshiefrazie@hotmail.com>
2026-03-17 01:38:06 -0400

    Fix more mobile bugs

--- Portfolio/ClientApp/src/app/app.component.ts
+++ Portfolio/ClientApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { GithubService } from '../app/services/github.service';
 
 @Component({
@@ -7,6 +7,13 @@ import { GithubService } from '../app/services/github.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Joshua John Frazer';
+
+  @ViewChild('bgVideo') bgVideo!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    const video = this.bgVideo.nativeElement;
+    video.addEventListener('ended', () => video.play());
+  }
 }
